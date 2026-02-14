1ebc01c44cb94321e8bca6068d44eb93e992da4f
Rafael Santana Luiz <rafael.s.luiz@gmail.com>
2026-02-13 22:45:01 -0300

    Mongo DB connection with repository and service

--- src/main/java/com/udemy/springmongo/resources/UserResource.java
+++ src/main/java/com/udemy/springmongo/resources/UserResource.java
@@ -1,9 +1,11 @@
 package com.udemy.springmongo.resources;
 
 import com.udemy.springmongo.domain.User;
+import com.udemy.springmongo.services.UserService;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,12 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@gmail.com");
-        List<User> usersList = new ArrayList<>();
-        usersList.addAll(Arrays.asList(maria, alex));
+        List<User> usersList = userService.findAll();
         return ResponseEntity.ok().body(usersList);
     }
 }
---
+++ src/main/java/com/udemy/springmongo/services/UserService.java
@@ -0,0 +1,18 @@
+package com.udemy.springmongo.services;
+
+import com.udemy.springmongo.domain.User;
+import com.udemy.springmongo.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+}
